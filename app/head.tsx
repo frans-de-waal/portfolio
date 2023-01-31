6bcb61ebc47718a0d4834a103fcfafb566fd3864
Frans de Waal <fmdewaal@gmail.com>
2023-01-31 23:59:17 +0200

    Init

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Portfolio - Frans de Waal</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Software development portfolio for Frans de Waal" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
