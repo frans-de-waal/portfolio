1a6aff06a0034ca0caaafc760dd44c3d4634468e
Frans de Waal <fmdewaal@gmail.com>
2023-02-02 21:51:51 +0200

    Added some font fallbacks

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,13 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Heading 1</h1>
-      <p>Ipsum amet laboris veniam aliqua in adipisicing id duis deserunt elit. Eu pariatur duis sint aliquip dolore dolor eu id velit culpa voluptate ex. Velit id adipisicing et elit proident incididunt tempor aute laborum laboris esse pariatur velit. Nulla qui Lorem cillum ullamco irure nostrud ea ullamco ad ex ipsum elit consectetur duis. Laborum eu deserunt deserunt occaecat aute qui anim laborum ex ullamco sint ex laboris dolor. Incididunt ex qui voluptate amet aute velit nisi.</p>
       <h2>Heading 2</h2>
-      <p>Ipsum amet laboris veniam aliqua in adipisicing id duis deserunt elit. Eu pariatur duis sint aliquip dolore dolor eu id velit culpa voluptate ex. Velit id adipisicing et elit proident incididunt tempor aute laborum laboris esse pariatur velit. Nulla qui Lorem cillum ullamco irure nostrud ea ullamco ad ex ipsum elit consectetur duis. Laborum eu deserunt deserunt occaecat aute qui anim laborum ex ullamco sint ex laboris dolor. Incididunt ex qui voluptate amet aute velit nisi.</p>
       <h3>Heading 3</h3>
-      <p>Ipsum amet laboris veniam aliqua in adipisicing id duis deserunt elit. Eu pariatur duis sint aliquip dolore dolor eu id velit culpa voluptate ex. Velit id adipisicing et elit proident incididunt tempor aute laborum laboris esse pariatur velit. Nulla qui Lorem cillum ullamco irure nostrud ea ullamco ad ex ipsum elit consectetur duis. Laborum eu deserunt deserunt occaecat aute qui anim laborum ex ullamco sint ex laboris dolor. Incididunt ex qui voluptate amet aute velit nisi.</p>
       <h4>Heading 4</h4>
-      <p>Ipsum amet laboris veniam aliqua in adipisicing id duis deserunt elit. Eu pariatur duis sint aliquip dolore dolor eu id velit culpa voluptate ex. Velit id adipisicing et elit proident incididunt tempor aute laborum laboris esse pariatur velit. Nulla qui Lorem cillum ullamco irure nostrud ea ullamco ad ex ipsum elit consectetur duis. Laborum eu deserunt deserunt occaecat aute qui anim laborum ex ullamco sint ex laboris dolor. Incididunt ex qui voluptate amet aute velit nisi.</p>
       <h5>Heading 5</h5>
-      <p>Ipsum amet laboris veniam aliqua in adipisicing id duis deserunt elit. Eu pariatur duis sint aliquip dolore dolor eu id velit culpa voluptate ex. Velit id adipisicing et elit proident incididunt tempor aute laborum laboris esse pariatur velit. Nulla qui Lorem cillum ullamco irure nostrud ea ullamco ad ex ipsum elit consectetur duis. Laborum eu deserunt deserunt occaecat aute qui anim laborum ex ullamco sint ex laboris dolor. Incididunt ex qui voluptate amet aute velit nisi.</p>
       <h6>Heading 6</h6>
       <p>Ipsum amet laboris veniam aliqua in adipisicing id duis deserunt elit. Eu pariatur duis sint aliquip dolore dolor eu id velit culpa voluptate ex. Velit id adipisicing et elit proident incididunt tempor aute laborum laboris esse pariatur velit. Nulla qui Lorem cillum ullamco irure nostrud ea ullamco ad ex ipsum elit consectetur duis. Laborum eu deserunt deserunt occaecat aute qui anim laborum ex ullamco sint ex laboris dolor. Incididunt ex qui voluptate amet aute velit nisi.</p>
+      <a href="somethingnice">Link</a>
     </main>
   )
 }
