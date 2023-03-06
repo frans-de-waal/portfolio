4062adc2b96b6527f44a8a033c31587e2ab850bd
Frans de Waal <fmdewaal@gmail.com>
2023-03-06 23:25:19 +0200

    Added some content to the Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,34 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import portrait from "../public/images/frans-transparent.png";
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div className={`${styles.row} ${styles.hero}`}>
+        <div className={styles.col}>
+          <h2>
+            Hi, my name is <strong>Frans</strong>.
+          </h2>
+          <h1>
+            I&apos;m a <strong>full stack developer</strong> specializing in{" "}
+            <strong>web and mobile apps</strong>.
+          </h1>
+        </div>
+        <div className={styles.circle}>
+          <Image
+            src={portrait}
+            alt="Portrait of Frans"
+            priority
+            width={400}
+            height={400}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.row}>
+        <h2>Featured Projects</h2>
+      </div>
     </>
   );
 }
