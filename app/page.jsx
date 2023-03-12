b41ab6040418040b017b47cb88cf1bedbf38e2ab
Frans de Waal <fmdewaal@gmail.com>
2023-03-12 12:34:43 +0200

    Minor styling changes

--- app/page.jsx
+++ app/page.jsx
@@ -4,35 +4,7 @@ import portrait from "../public/images/frans-transparent-2.png";
 import ProjectCard from "../components/ProjectCard";
 
 export default function Home() {
-  const PROJECTS = [
-    {
-      name: "OPFC Supporter App",
-      image: "/images/projects/opfc.svg",
-      summary:
-        "The official supporter app for one of South Africa's oldest football clubs Orlando Pirates.",
-      type: "Professional",
-      tech: "React Native",
-      links: {
-        android:
-          "https://play.google.com/store/apps/details?id=com.orlandopiratesfc.mobileapp",
-        ios: "https://apps.apple.com/za/app/orlando-pirates-official-app/id1469888955",
-        web: "",
-      },
-    },
-    {
-      name: "OPFC Supporter App",
-      image: "/images/projects/opfc.svg",
-      summary:
-        "The official supporter app for one of South Africa's oldest football clubs Orlando Pirates.",
-      type: "Professional",
-      tech: "React Native",
-      links: {
-        android:
-          "https://play.google.com/store/apps/details?id=com.orlandopiratesfc.mobileapp",
-        ios: "https://apps.apple.com/za/app/orlando-pirates-official-app/id1469888955",
-        web: "",
-      },
-    },
+  const FEATURED_PROJECTS = [
     {
       name: "OPFC Supporter App",
       image: "/images/projects/opfc.svg",
@@ -102,7 +74,7 @@ export default function Home() {
       </div>
       <h2>Featured Projects</h2>
       <ul className={styles.projectList}>
-        {PROJECTS.map((project) => {
+        {FEATURED_PROJECTS.map((project) => {
           const urlKey = project.name.toLowerCase().replace(/\s/g, "-");
           return <ProjectCard key={urlKey} urlKey={urlKey} {...project} />;
         })}
