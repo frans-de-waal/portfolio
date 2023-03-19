030526c64c5d91bfd371540f9365358a3ab5917f
Frans de Waal <fmdewaal@gmail.com>
2023-03-19 16:12:42 +0200

    Added some content to the About page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,3 +1,31 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <>
+      <h1>About Me</h1>
+      <p>
+        I&apos;m an experienced senior software engineer with a demonstrated
+        history of working with modern web technologies. I&apos;m passionate
+        about creating effective solutions for modern problems.
+      </p>
+      <h2>Work Experience</h2>
+      <h3>Vectra Business Technologies</h3>
+      <p>Senior Software Developer</p>
+      <h3>Comune.IT</h3>
+      <p>Development Team Lead</p>
+      <h2>Education</h2>
+      <h3>University of South Africa</h3>
+      <p>
+        Bachelor of Applied Science - Applied Mathematics and Computer Science
+      </p>
+      <h3>University of Pretoria</h3>
+      <p>Bachelor of Engineering - Electrical and Electronics Engineering</p>
+      <h2>Interests</h2>
+      <p>
+        I enjoy creating things, whether it&apos;s software, electronics or
+        making food.
+      </p>
+      <p>I also like playing games; board games, card games or PC games.</p>
+      <p>I like spending time with my wife and our two dogs.</p>
+    </>
+  );
 }
